872d44f3006f3edea6ce5faf5d619e8644104587
Pranav Vedula <vedulapranav@gmail.com>
2026-01-02 09:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (53.60%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int>seen;
+        for(int i:nums)
+        {
+            if(seen.count(i)){
+                return i;
+            }
+            seen.insert(i);
+
+        }
+        return 0;
+    }
+};
